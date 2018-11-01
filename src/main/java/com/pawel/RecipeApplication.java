457e0f1430abe162e0b49e2b03a0cdae45914a87
Pawel Pluta <pluta.p@gmail.com>
2018-11-01 19:55:13 +0100

    new comment example

--- src/main/java/com/pawel/RecipeApplication.java
+++ src/main/java/com/pawel/RecipeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RecipeApplication {
 
 	public static void main(String[] args) {
+		//comment
 		SpringApplication.run(RecipeApplication.class, args);
 	}
 }
